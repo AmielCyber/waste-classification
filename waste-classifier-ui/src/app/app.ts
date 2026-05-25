5724fcf8ce913b129465676df9f53305190bc92b
Amiel Nava <amieln@icloud.com>
2026-05-25 16:44:57 -0700

    Navigation Bar Skeleton

--- waste-classifier-ui/src/app/app.ts
+++ waste-classifier-ui/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { WasteBinComponent } from './waste-bin/waste-bin.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, WasteBinComponent],
+  imports: [RouterOutlet, WasteBinComponent, NavBarComponent],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
