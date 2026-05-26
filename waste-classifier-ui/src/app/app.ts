97469601426f24550bc5c42a74a0bb74b8b246bb
Amiel Nava <amieln@icloud.com>
2026-05-26 14:26:24 -0700

    Image Upload Component

--- waste-classifier-ui/src/app/app.ts
+++ waste-classifier-ui/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component} from '@angular/core';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { BinGridComponent } from './bin-grid/bin-grid.component';
+import { WasteImageUploadComponent } from './waste-image-upload/waste-image-upload.component';
 
 @Component({
   selector: 'app-root',
-  imports: [NavBarComponent, BinGridComponent],
+  imports: [NavBarComponent, BinGridComponent, WasteImageUploadComponent],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
