1c5ab1b1082bcec3fedb81121a80438aa6e2fe43
Amiel Nava <amieln@icloud.com>
2026-05-28 16:05:45 -0700

    Uses services instead of inputs

--- waste-classifier-ui/src/app/bin-grid/bin-grid.component.ts
+++ waste-classifier-ui/src/app/bin-grid/bin-grid.component.ts
@@ -1,6 +1,7 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject } from '@angular/core';
 import { WasteBinService } from '../services/waste-bin.service';
 import { WasteBinComponent } from '../waste-bin/waste-bin.component';
+import { ImageClassificationService } from '../services/image-classification.service';
 
 @Component({
   selector: 'app-bin-grid',
@@ -10,6 +11,13 @@ import { WasteBinComponent } from '../waste-bin/waste-bin.component';
 })
 export class BinGridComponent {
   private readonly wasteBinService = inject(WasteBinService);
+  private readonly imageClassifierService = inject(ImageClassificationService);
+
   protected readonly bins = this.wasteBinService.bins;
-  protected readonly binSelected = this.wasteBinService.binSelected;
+  protected readonly selectedBin = computed(() => {
+    if(this.imageClassifierService.prediction.hasValue()){
+      return this.imageClassifierService.prediction.value().type
+    }
+    return null
+  })
 }
