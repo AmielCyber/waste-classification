05e68bc4001d44eee567be380ee82f9baec2e2da
Amiel Nava <amieln@icloud.com>
2026-05-28 16:06:11 -0700

    Added Classification result to the page

--- waste-classifier-ui/src/app/app.ts
+++ waste-classifier-ui/src/app/app.ts
@@ -2,10 +2,18 @@ import { Component} from '@angular/core';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { BinGridComponent } from './bin-grid/bin-grid.component';
 import { WasteImageUploadComponent } from './waste-image-upload/waste-image-upload.component';
+import {
+  WasteClassificationResultComponent
+} from './waste-classification-result/waste-classification-result.component';
 
 @Component({
   selector: 'app-root',
-  imports: [NavBarComponent, BinGridComponent, WasteImageUploadComponent],
+  imports: [
+    NavBarComponent,
+    BinGridComponent,
+    WasteImageUploadComponent,
+    WasteClassificationResultComponent,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
