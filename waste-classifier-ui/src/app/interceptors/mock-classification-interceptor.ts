838d332d7afbf8df56479bb28afa8195d6becda0
Amiel Nava <amieln@icloud.com>
2026-05-28 16:05:11 -0700

    Added random mock result

--- waste-classifier-ui/src/app/interceptors/mock-classification-interceptor.ts
+++ waste-classifier-ui/src/app/interceptors/mock-classification-interceptor.ts
@@ -1,11 +1,33 @@
 import { HttpInterceptorFn, HttpResponse } from '@angular/common/http';
 import { ClassificationResult } from '../services/image-classification.service';
 import { delay, of } from 'rxjs';
+import { inject } from '@angular/core';
+import { WasteBin, WasteBinService, WasteType } from '../services/waste-bin.service';
+
+/**
+ * WILL GET REPLACED WITH REAL API CALL TO PERFORM AN ACTUAL CLASSIFICATION
+ */
+
+function getRandomWasteType(bins: readonly WasteBin[]): WasteType{
+  const binIndex = Math.floor(Math.random() * bins.length);
+  return bins[binIndex].type;
+}
+
+function getRandomConfidence(): number {
+  const min = 0.65;
+  const max = 0.98;
+  return Math.random() * (max - min) + min;
+}
 
 export const mockClassificationInterceptor: HttpInterceptorFn = (req, next) => {
+  const wastBinService = inject(WasteBinService);
+  const bins = wastBinService.bins;
   if (req.url.includes('api/predict-image') && req.method === 'POST') {
     console.log('Mocking classification request...');
-    const mockResponse: ClassificationResult = { type: 'Metal', confidence: 0.69 };
+    const mockResponse: ClassificationResult = {
+      type: getRandomWasteType(bins),
+      confidence: getRandomConfidence()
+    };
 
     return of(new HttpResponse({ status: 200, body: mockResponse })).pipe(delay(2000));
   }
