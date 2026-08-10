ab50e1755fe42e6f24013a7099fe78c2fc21a3f6
Amiel Nava <amieln@icloud.com>
2026-08-09 19:52:16 -0700

    upgrade to Angular 22

--- waste-classifier-ui/src/app/app.ts
+++ waste-classifier-ui/src/app/app.ts
@@ -1,10 +1,8 @@
-import { Component} from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { BinGridComponent } from './bin-grid/bin-grid.component';
 import { WasteImageUploadComponent } from './waste-image-upload/waste-image-upload.component';
-import {
-  WasteClassificationResultComponent
-} from './waste-classification-result/waste-classification-result.component';
+import { WasteClassificationResultComponent } from './waste-classification-result/waste-classification-result.component';
 
 @Component({
   selector: 'app-root',
@@ -15,6 +13,7 @@ import {
     WasteClassificationResultComponent,
   ],
   templateUrl: './app.html',
+  changeDetection: ChangeDetectionStrategy.Eager,
   styleUrl: './app.css',
 })
 export class App {}
